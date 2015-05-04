d9cb177684776d6310b2acdd72d29f97a07b1a4c
Eric Herbrandson <eric.herbrandson@q2ebanking.com>
2015-05-04 15:19:28 -0500

    Fixing a couple bugs. Adding support for devDependencies. Setting up package to have a simple bin script for use with global installs

--- index.js
+++ index.js
@@ -2,43 +2,75 @@ var fs = require('fs');
 var path = require('path');
 var semver = require('semver');
 var promise = require('bluebird');
+var eol = require('os').EOL;
+
+
+function _process(result, type) {
+	var isValid = (result.bad.length === 0);
+
+	if (!isValid) {
+		console.log('Missing ' + type + ' dependencies:');
+		result.bad.forEach(function(item) {
+			var message = item.name + eol + '  expected: ' + item.requested + eol + '  actual:   ' + (item.actual || 'missing') + eol;
+			console.log(message);
+		});
+	}
+
+	return isValid;
+}
 
 var self = {
-	checkNpm: function() {
-		return self.check('package.json', './node_modules');
+	hasNeededDependencies: function(includeDevDependencies) {
+		var isValid = true;
+
+		return self.checkNpm(includeDevDependencies)
+			.then(function(result) {
+				isValid &= _process(result, 'npm');
+
+				return self.checkBower(includeDevDependencies);
+			})
+			.then(function(result) {
+				isValid &= _process(result, 'bower');
+
+				return !!isValid;
+			});
 	},
 
-	checkBower: function() {
-		return self.check('bower.json', './bower_components');
+	checkNpm: function(includeDevDependencies) {
+		return self.check('package.json', './node_modules', includeDevDependencies);
 	},
 
-	check: function(package, root) {
+	checkBower: function(includeDevDependencies) {
+		return self.check('bower.json', './bower_components', includeDevDependencies);
+	},
+
+	check: function(package, root, includeDevDependencies) {
 		var installed;
 
 		return promise.try(function() {
-				return self.getInstalledPackages(root);
+				return self.getInstalledPackages(root, package);
 			})
 			.then(function(data) {
 				installed = data;
-				return self.getRequiredPackages(package);
+				return self.getRequiredPackages(package, includeDevDependencies);
 			})
 			.then(function(requested) {
 				return self.getStatus(requested, installed);
 			});
 	},
 
-	getInstalledPackages: function(root) {
+	getInstalledPackages: function(root, package) {
+		// TODO: make this all async
+
 		if (!fs.existsSync(root)) {
-			return {};
+			return {notFound:root};
 		}
 
 		return promise.promisify(fs.readdir)(root)
 			.then(function(data) {
-				// TODO: make this all async
 				var installed = {};
-
 				data.forEach(function(directory) {
-					var packagePath = path.join(root, directory, 'package.json');
+					var packagePath = path.join(root, directory, package);
 
 					if (!fs.existsSync(packagePath)) { return; }
 
@@ -51,14 +83,27 @@ var self = {
 			});
 	},
 
-	getRequiredPackages: function(package) {
+	getRequiredPackages: function(package, includeDevDependencies) {
 		if (!fs.existsSync(package)) {
 			return {};
 		}
 
 		return promise.promisify(fs.readFile)(package, 'utf8')
 			.then(function(data) {
-				return JSON.parse(data).dependencies;
+				var result = {};
+				var json = JSON.parse(data);
+
+				if (includeDevDependencies) {
+					for (var devKey in json.devDependencies) {
+						result[devKey] = json.devDependencies[devKey];
+					}
+				}
+
+				for (var key in json.dependencies) {
+					result[key] = json.dependencies[key];
+				}
+
+				return result;
 			});
 	},
 
@@ -69,7 +114,7 @@ var self = {
 			var dependency = {
 				name: name,
 				requested: requested[name],
-				actual: (installed[name]) ? installed[name] : '',
+				actual: installed[name],
 			};
 
 			var isValid = semver.satisfies(dependency.actual, dependency.requested);
